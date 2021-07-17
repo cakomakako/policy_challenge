aa4292bee3a6a1468af0ff738ce18ecb00632057
Eric Hernandez <eric.vim.root@gmail.com>
2021-07-17 23:40:46 +0000

    busted atm

--- policy.tf
+++ policy.tf
@@ -1,3 +1,21 @@
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role_policy" "test_policy" {
   name = "test_policy"
   role = aws_iam_role.test_role.id
@@ -16,24 +34,6 @@ resource "aws_iam_role_policy" "test_policy" {
         ]
         Effect   = "Allow"
         Resource = "*"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role" "test_role" {
-  name = "test_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
       },
     ]
   })
