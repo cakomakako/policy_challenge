a32dba3b0616b07624133cdbb0a27ba58b40b32d
Eric Hernandez <eric.vim.root@gmail.com>
2021-07-18 00:21:32 +0000

    Still messed up

--- ec2.tf
+++ ec2.tf
@@ -11,8 +11,36 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "ssh to VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "tf-example"
@@ -21,7 +49,7 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-2a"
 
   tags = {
@@ -31,7 +59,7 @@ resource "aws_subnet" "my_subnet" {
 
 resource "aws_network_interface" "foo" {
   subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+  private_ips = ["10.0.1.17"]
 
   tags = {
     Name = "primary_network_interface"
@@ -56,7 +84,8 @@ resource "aws_instance" "foo" {
   instance_type        = "t2.micro"
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
   key_name             = aws_key_pair.deployer.key_name
-
+  subnet_id            = aws_subnet.my_subnet.id
+  security_groups      = [aws_security_group.allow_ssh.id]
   network_interface {
     network_interface_id = aws_network_interface.foo.id
     device_index         = 0
@@ -78,5 +107,9 @@ resource "aws_eip" "example" {
 
 output "public_ip" {
   value = aws_eip.example.*
+}
+
+output "ec2" {
+  value = aws_instance.foo.*
 }
 
