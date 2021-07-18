609acd638b7ff1f1bc31c108dafedeb20d3da7d1
Eric Hernandez <eric.vim.root@gmail.com>
2021-07-18 11:54:36 +0000

    Added s3 terraform

---
+++ s3.tf
@@ -0,0 +1,41 @@
+resource "aws_kms_key" "a" {
+  description = "KMS key 1"
+}
+
+variable "s3_id" {
+  type    = string
+  default = "first_name"
+
+}
+
+
+resource "random_pet" "s3" {
+  keepers = {
+    # Generate a new pet name each time we switch to a new AMI id
+    s3 = var.s3_id
+  }
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = random_pet.s3.id
+  acl    = "private"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.a.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+output "s3" {
+  value = aws_s3_bucket.b.*
+}
+
